79f2bf5f634f8cdad7bbcbbdcf425c67d19704b4
Roberta Fagandini <robifagandini@hotmail.it>
2022-03-24 17:04:04 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -182,31 +182,31 @@ StandaloneHTMLBuilder.supported_image_types = [
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     #
+#     # 'papersize': 'letterpaper',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
+#     # The font size ('10pt', '11pt' or '12pt').
+#     #
+#     # 'pointsize': '10pt',
 
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
+#     # Additional stuff for the LaTeX preamble.
+#     #
+#     # 'preamble': '',
 
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
+#     # Latex figure (float) alignment
+#     #
+#     # 'figure_align': 'htbp',
+# }
 
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'manuale_titolo_no_spazi.tex', u'Manuale - TITOLO', ######## DA MODIFICARE########
-     u'Gter srl', 'manual'),
-]
+# # Grouping the document tree into LaTeX files. List of tuples
+# # (source start file, target name, title,
+# #  author, documentclass [howto, manual, or own class]).
+# latex_documents = [
+#     (master_doc, 'manuale_titolo_no_spazi.tex', u'Manuale - TITOLO', ######## DA MODIFICARE########
+#      u'Gter srl', 'manual'),
+# ]
 
 
 # -- Options for manual page output ------------------------------------------
